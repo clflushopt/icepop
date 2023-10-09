f438099e677e32565fa0a2d8163487414dc93163
jmpnz <dumpsterdev@gmail.com>
2023-10-09 00:24:47 -0400

    feat: basic instruction decoder and a bunch of instructions

--- src/emu.rs
+++ src/emu.rs
@@ -1,5 +1,7 @@
 //! 64-bit RISC-V RV64i emulator.
-use crate::machine::{Register, MAX_CPU_REGISTERS};
+use crate::machine::{
+    Btype, Itype, Jtype, Register, Rtype, Stype, Utype, MAX_CPU_REGISTERS,
+};
 use crate::mmu::{Mmu, Permission, VirtAddr, DEFAULT_EMU_MMU_SIZE, PERM_EXEC};
 
 /// Single threaded RISC-V RV64i emulator with an MMU.
@@ -28,12 +30,18 @@ impl Emulator {
 
     /// Read a register.
     pub fn reg(&self, register: Register) -> u64 {
-        self.registers[register as usize]
+        if register != Register::Zero {
+            self.registers[register as usize]
+        } else {
+            0
+        }
     }
 
     /// Set a register to `value`.
     pub fn set_reg(&mut self, register: Register, value: u64) {
-        self.registers[register as usize] = value
+        if register != Register::Zero {
+            self.registers[register as usize] = value
+        }
     }
 
     /// Run the emulator loop.
@@ -50,12 +58,272 @@ impl Emulator {
             // Decode instruction.
             let opcode = inst & 0b1111111;
 
+            println!("Executing  {:08b} @ {:08x}\n", opcode, pc);
+
             match opcode {
                 0b0110111 => {
                     // LUI.
+                    let inst = Utype::from(inst);
+                    self.set_reg(inst.rd, inst.imm as i64 as u64);
+                    print!("{:?}\n", inst);
+                }
+                0b0010111 => {
+                    // AUIPC.
+                    let inst = Utype::from(inst);
+                    self.set_reg(
+                        inst.rd,
+                        (inst.imm as i64 as u64).wrapping_add(pc),
+                    );
+                }
+                0b1101111 => {
+                    // JAL.
+                    let inst = Jtype::from(inst);
+                    self.set_reg(inst.rd, pc.wrapping_add(4));
+                    self.set_reg(
+                        Register::Pc,
+                        pc.wrapping_add(inst.imm as i64 as u64),
+                    )
+                }
+                0b1100111 => {
+                    // Itype instruction.
+                    let inst = Itype::from(inst);
+                    match inst.funct3 {
+                        0b000 => {
+                            // JALR.
+                            // Target address of JALR.
+                            let target = self
+                                .reg(inst.rs1)
+                                .wrapping_add(inst.imm as i64 as u64);
+                            // Set pc to target address.
+                            self.set_reg(inst.rd, pc.wrapping_add(4));
+                            self.set_reg(Register::Pc, target);
+                        }
+
+                        _ => todo!(),
+                    }
+                }
+                0b11000111 => {
+                    // Btype instruction.
+                    let inst = Btype::from(inst);
+
+                    let rs1 = self.reg(inst.rs1);
+                    let rs2 = self.reg(inst.rs2);
+                    match inst.funct3 {
+                        0b000 => {
+                            // BEQ
+                            if rs1 == rs2 {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        0b001 => {
+                            // BNE
+                            if rs1 != rs2 {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        0b100 => {
+                            // BLT
+                            if (rs1 as i64) < (rs2 as i64) {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        0b101 => {
+                            // BGE
+                            if (rs1 as i64) >= (rs2 as i64) {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        0b110 => {
+                            // BLTU
+                            if (rs1 as u64) < (rs2 as u64) {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        0b111 => {
+                            // BGEU
+                            if (rs1 as u64) >= (rs2 as u64) {
+                                self.set_reg(
+                                    Register::Pc,
+                                    pc.wrapping_add(inst.imm as i64 as u64),
+                                );
+                            }
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                0b0000011 => {
+                    // Itype instruction.
+                    let inst = Itype::from(inst);
+                    // Address to load from.
+                    let addr = VirtAddr(
+                        self.reg(inst.rs1).wrapping_add(inst.imm as i64 as u64)
+                            as usize,
+                    );
+                    match inst.funct3 {
+                        0b000 => {
+                            // LB.
+                            let mut b = [0u8; 1];
+                            self.memory.read(addr, &mut b)?;
+                            self.set_reg(
+                                inst.rd,
+                                i8::from_le_bytes(b) as i64 as u64,
+                            );
+                        }
+                        0b001 => {
+                            // LH.
+                            let mut b = [0u8; 2];
+                            self.memory.read(addr, &mut b)?;
+                            self.set_reg(
+                                inst.rd,
+                                i16::from_le_bytes(b) as i64 as u64,
+                            );
+                        }
+                        0b010 => {
+                            // LW.
+                            let mut b = [0u8; 4];
+                            self.memory.read(addr, &mut b)?;
+                            self.set_reg(
+                                inst.rd,
+                                i32::from_le_bytes(b) as i64 as u64,
+                            );
+                        }
+                        0b100 => {
+                            // LBU.
+                            let mut b = [0u8; 1];
+                            self.memory.read(addr, &mut b)?;
+                            self.set_reg(inst.rd, u8::from_le_bytes(b) as u64);
+                        }
+                        0b101 => {
+                            // LHU.
+                            let mut b = [0u8; 2];
+                            self.memory.read(addr, &mut b)?;
+                            self.set_reg(inst.rd, u16::from_le_bytes(b) as u64);
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                0b0100011 => {
+                    // S-type instruction.
+                    let inst = Stype::from(inst);
+                    // Compute target address.
+                    let addr = VirtAddr(
+                        self.reg(inst.rs1).wrapping_add(inst.imm as i64 as u64)
+                            as usize,
+                    );
+
+                    match inst.funct3 {
+                        0b000 => {
+                            // SB.
+                            let val = self.reg(inst.rs2) as u8;
+                            self.memory.write_into(addr, val)?;
+                        }
+                        0b001 => {
+                            // SH.
+                            let val = self.reg(inst.rs2) as u16;
+                            self.memory.write_into(addr, val)?;
+                        }
+                        0b010 => {
+                            // SW.
+                            let val = self.reg(inst.rs2) as u32;
+                            self.memory.write_into(addr, val)?;
+                        }
+                        _ => todo!(),
+                    }
+                }
+                0b0010011 => {
+                    // I-type instruction.
+                    let inst = Itype::from(inst);
+
+                    let rs1 = self.reg(inst.rs1);
+                    let imm = inst.imm as i64 as u64;
+
+                    match inst.funct3 {
+                        0b000 => {
+                            // ADDI
+                            self.set_reg(inst.rd, rs1.wrapping_add(imm));
+                        }
+                        0b010 => {
+                            // SLTI
+                            if (rs1 as i64) < (imm as i64) {
+                                self.set_reg(inst.rd, 1);
+                            } else {
+                                self.set_reg(inst.rd, 0);
+                            }
+                        }
+                        0b011 => {
+                            // SLTIU
+                            if (rs1 as u64) < (imm as u64) {
+                                self.set_reg(inst.rd, 1);
+                            } else {
+                                self.set_reg(inst.rd, 0);
+                            }
+                        }
+                        0b100 => {
+                            // XORI
+                            self.set_reg(inst.rd, rs1 ^ imm);
+                        }
+                        0b110 => {
+                            // ORI
+                            self.set_reg(inst.rd, rs1 | imm);
+                        }
+                        0b111 => {
+                            // ANDI
+                            self.set_reg(inst.rd, rs1 & imm);
+                        }
+                        0b001 => {
+                            // SLLI
+                            let shamt = inst.imm & 0b111111;
+                            self.set_reg(inst.rd, rs1 << shamt);
+                        }
+                        0b101 => {
+                            let mode = (inst.imm >> 6) & 0b111111;
+
+                            match mode {
+                                0b0000000 => {
+                                    // SRLI
+                                    let shamt = inst.imm & 0b111111;
+                                    self.set_reg(inst.rd, rs1 >> shamt);
+                                }
+                                0b0100000 => {
+                                    // SRAI
+                                    let shamt = inst.imm & 0b111111;
+                                    self.set_reg(
+                                        inst.rd,
+                                        ((rs1 as i64) >> shamt) as u64,
+                                    );
+                                }
+                                _ => unreachable!(),
+                            }
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                0b0110011 => {
+                    let inst = Rtype::from(inst);
+
+                    match inst.funct3 {
+                        _ => todo!(),
+                    }
                 }
                 _ => todo!(),
             }
+            // Increment PC to the next instruction.
+            self.set_reg(Register::Pc, pc.wrapping_add(4));
         }
     }
 }
--- src/machine.rs
+++ src/machine.rs
@@ -43,16 +43,16 @@ pub enum Register {
     Pc,
 }
 
-impl From<u8> for Register {
-    /// Transform a `u8` to `Register`, this cast is useful when parsing
-    /// instructions that encoded source and destination registers.
+impl From<u32> for Register {
+    /// Transforms a `u32` to `Register`, this cast is useful when parsing
+    /// instructions that encode source and destination registers.
     ///
     /// # Safety
     ///
     /// Caller must ensure that value is within the specified range [0..32)
     /// otherwise the assertion will fail.
     /// Since we use `#[repr(usize)]` the pointer cast is safe.
-    fn from(value: u8) -> Self {
+    fn from(value: u32) -> Self {
         assert!(value < 32);
         unsafe {
             core::ptr::read_unaligned(
@@ -63,7 +63,145 @@ impl From<u8> for Register {
 }
 
 /// U-type instruction.
+#[derive(Debug, Clone, Copy)]
 pub struct Utype {
-    imm: i64,
-    rd: Register,
+    pub imm: i64,
+    pub rd: Register,
+}
+
+impl From<u32> for Utype {
+    /// Decode a `Utype` instruction.
+    fn from(inst: u32) -> Self {
+        Self {
+            imm: (inst & !0xfff) as i32 as i64,
+            rd: Register::from((inst >> 7) & 0b11111),
+        }
+    }
+}
+
+/// J-type instruction.
+#[derive(Debug, Clone, Copy)]
+pub struct Jtype {
+    pub imm: i32,
+    pub rd: Register,
+}
+
+impl From<u32> for Jtype {
+    /// Decode a `Jtype` instruction.
+    fn from(inst: u32) -> Self {
+        let imm20 = (inst >> 31) & 1;
+        let imm101 = (inst >> 21) & 0b1111111111;
+        let imm11 = (inst >> 20) & 1;
+        let imm1912 = (inst >> 12) & 0b11111111;
+        let imm =
+            (imm20 << 20) | (imm1912 << 12) | (imm11 << 11) | (imm101 << 1);
+        let imm = ((imm as i32) << 11) >> 11;
+
+        Self {
+            imm: imm,
+            rd: Register::from((inst >> 7) & 0b11111),
+        }
+    }
+}
+
+/// I-type instruction.
+#[derive(Debug, Clone, Copy)]
+pub struct Itype {
+    pub imm: i32,
+    pub rs1: Register,
+    pub funct3: u32,
+    pub rd: Register,
+}
+
+impl From<u32> for Itype {
+    /// Decode a `Itype` instruction.
+    fn from(inst: u32) -> Self {
+        let imm = (inst as i32) >> 20;
+        let rs1 = Register::from((inst >> 15) & 0b11111);
+        let rd = Register::from((inst >> 7) & 0b11111);
+        let funct3 = (inst >> 12) & 0b111;
+
+        Self {
+            imm,
+            rs1,
+            funct3,
+            rd,
+        }
+    }
+}
+
+/// B-type instruction.
+#[derive(Debug, Clone, Copy)]
+pub struct Btype {
+    pub imm: i32,
+    pub rs2: Register,
+    pub rs1: Register,
+    pub funct3: u32,
+}
+
+impl From<u32> for Btype {
+    /// Decode a `Btype` instruction.
+    fn from(inst: u32) -> Self {
+        let imm12 = (inst >> 31) & 1;
+        let imm105 = (inst >> 25) & 0b111111;
+        let imm41 = (inst >> 8) & 0b1111;
+        let imm11 = (inst >> 7) & 1;
+        let imm = (imm12 << 12) | (imm11 << 11) | (imm105 << 5) | (imm41 << 1);
+        let imm = ((imm as i32) << 19) >> 19;
+
+        Self {
+            imm: imm,
+            rs2: Register::from((inst >> 20) & 0b11111),
+            rs1: Register::from((inst >> 15) & 0b11111),
+            funct3: (inst >> 12) & 0b111,
+        }
+    }
+}
+
+/// R-type instruction.
+#[derive(Debug, Clone, Copy)]
+pub struct Rtype {
+    pub rs2: Register,
+    pub rs1: Register,
+    pub rd: Register,
+    pub funct3: u32,
+    pub funct7: u32,
+}
+
+impl From<u32> for Rtype {
+    /// Decode a `Rtype` instruction.
+    fn from(inst: u32) -> Self {
+        Self {
+            rs2: Register::from((inst >> 20) & 0b11111),
+            rs1: Register::from((inst >> 15) & 0b11111),
+            rd: Register::from((inst >> 7) & 0b11111),
+            funct3: (inst >> 12) & 0b111,
+            funct7: (inst >> 25) & 0b1111111,
+        }
+    }
+}
+
+/// S-type instruction.
+#[derive(Debug, Clone, Copy)]
+pub struct Stype {
+    pub imm: i32,
+    pub rs2: Register,
+    pub rs1: Register,
+    pub funct3: u32,
+}
+
+impl From<u32> for Stype {
+    /// Decode a `Stype` instruction.
+    fn from(inst: u32) -> Self {
+        let imm115 = (inst >> 25) & 0b1111111;
+        let imm40 = (inst >> 7) & 0b11111;
+        let imm = (imm115 << 5) | (imm40);
+        let imm = ((imm as i32) << 20) >> 20;
+        Self {
+            imm: imm,
+            rs2: Register::from((inst >> 20) & 0b11111),
+            rs1: Register::from((inst >> 15) & 0b11111),
+            funct3: (inst >> 12) & 0b111,
+        }
+    }
 }
